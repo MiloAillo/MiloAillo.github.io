632caef4b0ea1ad379cc040a5aea8aef2662cde8
FarisKa <109688841+MiloAillo@users.noreply.github.com>
2024-07-27 17:20:58 +0700

    Links Update
    Created library for people to see my project easily.

---
+++ Library.js
@@ -0,0 +1,41 @@
+const detail = document.getElementById("summary");
+
+detail.addEventListener("mouseover", mouseOver);
+detail.addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  detail.open = true;
+}
+
+function mouseOut() {
+  detail.open = false;
+}
+// My Bio Project 
+
+const detail2 = document.getElementById("summary2");
+
+detail2.addEventListener("mouseover", mouseOver2);
+detail2.addEventListener("mouseout", mouseOut2);
+
+function mouseOver2() {
+  detail2.open = true;
+}
+
+function mouseOut2() {
+  detail2.open = false;
+}
+// Length Converter Project
+
+const detail3 = document.getElementById("summary3");
+
+detail3.addEventListener("mouseover", mouseOver3);
+detail3.addEventListener("mouseout", mouseOut3);
+
+function mouseOver3() {
+  detail3.open = true;
+}
+
+function mouseOut3() {
+  detail3.open = false;
+}
+// *Notes
